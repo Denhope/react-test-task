c2a0948b6fe2dff0e58401d2e6504251fff34abb
den_hope <den_hope@mail.ru>
2022-07-07 19:04:32 +0300

    feat: add reestr store

---
+++ src/store/reducers/reestrs/types.ts
@@ -0,0 +1,36 @@
+export interface ReestrState {
+  reesrtData: Array<any>;
+  isLoading: boolean;
+  error: null | string;
+}
+
+export enum ReestrActionEnum {
+  REQUEST_REESTR = 'REQUEST_REESTR',
+  RECEIVE_REESTR_SUCCESS = 'RECEIVE_REESTR_SUCCESS',
+  RECEIVE_REESTR_ERROR = 'RECEIVE_REESTR_ERROR',
+  SET_IS_LOADING = 'SET_IS_LOADING',
+}
+
+export interface RequestReestrAction {
+  type: ReestrActionEnum.REQUEST_REESTR;
+}
+
+export interface RecieveReestrAction {
+  type: ReestrActionEnum.RECEIVE_REESTR_SUCCESS;
+  payload: Array<any>;
+}
+
+export interface RecieveReestrErrorAction {
+  type: ReestrActionEnum.RECEIVE_REESTR_ERROR;
+  payload: string;
+}
+
+export interface SetIsLoadingAction {
+  type: ReestrActionEnum.SET_IS_LOADING;
+  payload: boolean;
+}
+export type ReestrAction =
+  | RequestReestrAction
+  | RecieveReestrAction
+  | RecieveReestrErrorAction
+  | SetIsLoadingAction;

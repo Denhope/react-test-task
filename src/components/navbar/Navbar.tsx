f68e82094af8b982b493afd0d2a0bb7480f141db
den_hope <den_hope@mail.ru>
2022-07-07 10:49:43 +0300

    feat: update navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,13 @@
-import { Avatar, Image, Layout, Menu, PageHeader, Row, Space } from 'antd';
+import { Avatar, Divider, Image, Layout, Menu, PageHeader, Row, Space } from 'antd';
 import Search from 'antd/lib/input/Search';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useActions } from '../../hooks/usections';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
 import { RouteNames } from '../../router';
 import './Navbar.scss';
 
-import { NotesCalendarIcon, BellIcon, LineIcon } from '../../components/icons/icons';
+import { NotesCalendarIcon, BellIcon, LineIcon, LogoIcon } from '../../components/icons/icons';
 import { UserOutlined } from '@ant-design/icons';
 
 const Navbar: FC = () => {
@@ -20,56 +20,52 @@ const Navbar: FC = () => {
   const { isAuth, user } = useTypedSelector((state) => state.auth);
 
   return (
-    <Layout>
-      <PageHeader
-        className="site-page-header"
-        title="Реестр ПО"
-        onBack={() => null}
-        extra={[
-          <Row align="middle">
-            <Search placeholder="поиск" onSearch={() => null} style={{ maxWidth: 200 }} />
-            <Row align="middle">
-              <Space align="center" size={20}>
-                <NotesCalendarIcon style={{ fontSize: '32px' }} />
-                <BellIcon style={{ fontSize: '32px' }} />
-                <LineIcon />
-                <Space>
-                  {isAuth ? (
-                    <Space align="center" size={10}>
-                      <Avatar
-                        src={
-                          <Image src="https://joeschmoe.io/api/v1/random" style={{ width: 32 }} />
-                        }
-                      />
+    <Row className="header-content">
+      <Row align="middle" className="header-left">
+        <LogoIcon />
+        <Search placeholder="поиск" onSearch={() => null} style={{ maxWidth: 200 }} />
+      </Row>
+      <Row align="middle" className="header-end">
+        <Space align="center" size={20}>
+          <Row align="middle" className="header-notification">
+            <NotesCalendarIcon style={{ fontSize: '32px' }} />
+            <BellIcon style={{ fontSize: '32px' }} />
+            <LineIcon />
+          </Row>
 
-                      <Menu>
-                        <Menu.SubMenu key="SubMenu" title={user.username}>
-                          <Menu.Item onClick={exit} key={1}>
-                            Выйти
-                          </Menu.Item>
-                        </Menu.SubMenu>
-                      </Menu>
-                    </Space>
-                  ) : (
-                    <Space align="center" size={10}>
-                      <Avatar size={32} icon={<UserOutlined />} />
+          <>
+            {isAuth ? (
+              <Space align="center" size={10}>
+                <Avatar src={<Image src={user.photoUrl} style={{ width: 32 }} />} />
 
-                      <Menu>
-                        <Menu.SubMenu key="SubMenu" title="Вход не выполнен">
-                          <Menu.Item onClick={() => router.push(RouteNames.LOGIN)} key={2}>
-                            Авторизоваться
-                          </Menu.Item>
-                        </Menu.SubMenu>
-                      </Menu>
-                    </Space>
-                  )}
-                </Space>
+                <Menu>
+                  <Menu.SubMenu key="SubMenu" title={user.firstname}>
+                    <Menu.Item onClick={exit} key={1}>
+                      Выйти
+                    </Menu.Item>
+                    <Menu.Item onClick={() => router.push(RouteNames.PERSONALCABINET_INFO)} key={2}>
+                      Личный кабинет
+                    </Menu.Item>
+                  </Menu.SubMenu>
+                </Menu>
               </Space>
-            </Row>
-          </Row>,
-        ]}
-      />
-    </Layout>
+            ) : (
+              <Space align="center" size={10}>
+                <Avatar size={32} icon={<UserOutlined />} />
+
+                <Menu>
+                  <Menu.SubMenu key="SubMenu" title="Вход в аккаунт">
+                    <Menu.Item onClick={() => router.push(RouteNames.LOGIN)} key={2}>
+                      Авторизоваться
+                    </Menu.Item>
+                  </Menu.SubMenu>
+                </Menu>
+              </Space>
+            )}
+          </>
+        </Space>
+      </Row>
+    </Row>
   );
 };
 
